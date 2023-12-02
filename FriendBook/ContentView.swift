6c36a0e5519601e5b7a223f894e631ae3bd1896a
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-12-03 02:30:01 +0600

    Initial Commit

---
+++ FriendBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendBook
+//
+//  Created by A.f. Adib on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FriendBook/FriendBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendBookApp.swift
+//  FriendBook
+//
+//  Created by A.f. Adib on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
